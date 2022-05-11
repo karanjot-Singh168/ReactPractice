a367abb200f384d60bbed959b7309f49b47212e2
Rajat <rajat.chugh2431@gmail.com>
2022-05-11 17:39:39 +0530

    extracting Input logic to separate component

---
+++ src/components/input.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+export default function Input() {
+  const [firstInputValue, setFirstInputValue] = useState("");
+  const [ShowFirstInputValue, setShowFirstInputValue] = useState(false);
+  const [secondInputValue, setSecondInputValue] = useState("");
+  return (
+    <div className="App">
+      {/* First input */}
+      <div className="first">
+        <input
+          type="text"
+          value={firstInputValue}
+          onChange={(e) => setFirstInputValue(e.target.value)}
+        />
+        <button onClick={() => setShowFirstInputValue(true)}>Show</button>
+        <label>{ShowFirstInputValue ? firstInputValue : ""}</label>
+      </div>
+      <hr />
+
+      {/* Second input  */}
+      <div className="second">
+        <input
+          value={secondInputValue}
+          onChange={(e) => setSecondInputValue(e.target.value)}
+          type="text"
+        />
+        <label>{secondInputValue}</label>
+      </div>
+    </div>
+  );
+}
