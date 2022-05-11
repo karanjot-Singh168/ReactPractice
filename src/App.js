3deb56d7bd25ddc00456422421560b0bb5ed683c
Rajat <rajat.chugh2431@gmail.com>
2022-05-11 16:48:18 +0530

    first assignment completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [firstInputValue, setFirstInputValue] = useState("");
+  const [ShowFirstInputValue, setShowFirstInputValue] = useState(false);
+  const [secondInputValue, setSecondInputValue] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* First input */}
+      <div className="first">
+        <input
+          type="text"
+          value={firstInputValue}
+          onChange={(e) => setFirstInputValue(e.target.value)}
+        />
+        <button onClick={() => setShowFirstInputValue(true)}>Show</button>
+        <label>{ShowFirstInputValue ? firstInputValue : ""}</label>
+      </div>
+      <hr />
+
+      {/* Second input  */}
+      <div className="second">
+        <input
+          value={secondInputValue}
+          onChange={(e) => setSecondInputValue(e.target.value)}
+          type="text"
+        />
+        <label>{secondInputValue}</label>
+      </div>
     </div>
   );
 }
